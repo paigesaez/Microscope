11f9ea18b6c294c7ad3ffda855941c84fcf9a7bf
paigesaez <paigesaez@gmail.com>
2015-06-03 11:15:54 -0500

    Revert "sidebar on latency compensation to expose what the client and the server do with data input on the fly"
    This reverts commit c5256d42f47e158bfe0fedef4ce33c7e06f514d8.

--- client/templates/posts/post_submit.js
+++ client/templates/posts/post_submit.js
@@ -7,19 +7,14 @@ Template.postSubmit.events({
       title: $(e.target).find('[name=title]').val()
     };
 
-    Meteor.call('postInsert', post, function(error, result)
-    {
-    // display the error to the user and abort
+   Meteor.call('postInsert', post, function(error, result)
+   {
+    //display the error to the user and abort
     if (error)
       return alert(error.reason);
 
-    // show this result but route anyway
-    if (result.postExists)
-      alert('This link is already here yo!');
+      Router.go('postPage', {_id: result._id});
     });
-
-      Router.go('postsList');
-
   }
 });
 
