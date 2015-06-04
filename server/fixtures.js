9af153b05d9ac7b4b960dbaee43e63c0562033f7
paigesaez <paigesaez@gmail.com>
2015-06-04 13:52:43 -0500

    added comments collection, publish and subscribe fixtures

--- server/fixtures.js
+++ server/fixtures.js
@@ -1,8 +1,33 @@
+// Fixture data
 if (Posts.find().count() === 0) {
-Posts.insert({
-title: 'Introducing Telescope',
-url: 'http://sachagreif.com/introducing-telescope/'
+  var now = new Date().getTime();
+
+// create two users
+var tomId = Meteor.users.insert({
+  profile: { name: 'Tom Cat'}
 });
+var tom = Meteor.users.findOne(tomId);
+var jerryId = Meteor.users.insert({
+  profile: { name: 'Jerry'}
+});
+var jerry = Meteor.users.findOne(jerryId);
+
+var telescopeId = Posts.insert({
+  title: 'Introducing Telescope',
+  userId: 'jerry._id',
+  author: jerry.profile.name,
+  url: 'http://google.com',
+  submitted: new Date(now - 7 * 3600 * 1000)
+});
+
+Comments.insert({
+  postId: telescopeId,
+  userId: tom._id,
+  author: tom.profile.name,
+  submitted: new Date(now - 5 * 3600 * 1000),
+  body: 'Interesting project...'
+});
+
 
 Posts.insert({
 title: 'Meteor',
@@ -13,7 +38,7 @@ Posts.insert({
 title: 'The Meteor Book',
 url: 'http://themeteorbook.com'
 });
-}    
+}
 
 
 
